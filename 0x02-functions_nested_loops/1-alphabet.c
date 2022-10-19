16980120b3b8b8f9eb8af671f09a7dcd4fe3a5d9
herits01 <israeletsano@gmail.com>
2022-10-19 10:11:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
  * print_alphabet - prints all alphabets in lowercase.
  * Return: On success 1.
  **/
-void print_alphabet(void)
+	void print_alphabet(void)
 {
-		char i;
-	for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-					}
+	char i;
+		for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+				}
 	_putchar('\n');
 
 }
