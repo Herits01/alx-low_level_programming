716d240f0b9ff20f72b3a662fc5e059ed38d2b21
herits01 <israeletsano@gmail.com>
2022-10-18 17:35:43 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+
+	/** main: data entry point
+	*Description: prints _putchar
+	* Return: always 0 (success) **/
+int _putchar (int char)
+{
+	int putchar (int char)
+	return (0)
+}
