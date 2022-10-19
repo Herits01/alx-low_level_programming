85ffae5f2f1e2d16c168e697ae739f02603a763c
herits01 <israeletsano@gmail.com>
2022-10-19 07:49:11 -0700

    Coding that prints _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef main_h
-#define <stdio.h>
 #define main_h
 extern int _putchar(char c);
-int _putchar (int x);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
@@ -15,4 +13,4 @@ extern void times_table(void);
 extern int add(int, int);
 extern void print_to_98(int n);
 extern void print_times_table(int n);
-#endif
+#endiff
