8757ffaece08a6008ca7cbeb0c57a32a2168dac6
herits01 <israeletsano@gmail.com>
2022-10-19 08:42:37 -0700

    test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef main_h
 #define main_h
 extern int _putchar(char c);
-int _putchar (int x)
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
