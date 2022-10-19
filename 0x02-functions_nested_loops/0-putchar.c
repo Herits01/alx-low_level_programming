3f6fc4198b5a34390b415f204d45d0de7cabf0b3
herits01 <israeletsano@gmail.com>
2022-10-19 10:38:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  *  main - data entry point
- * return: always return (0) **/
+ *  return: always return (0) 
+ **/
 int main(void)
-{
-	_putchar('_');
-	putchar('p');
-	_putchar('u');
-	putchar('t');
-	_putchar('c');
-	putchar('h');
-	_putchar('a');
-	putchar('r');
-	_putchar('\n');
+	{
+		_putchar('_');
+		putchar('p');
+		_putchar('u');
+		putchar('t');
+		_putchar('c');
+		putchar('h');
+		_putchar('a');
+		putchar('r');
+		_putchar('\n');
 return (0);
-}
+				}
