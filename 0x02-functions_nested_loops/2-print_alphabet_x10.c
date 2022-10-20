eca6774fc617bff4d6b8ee2743bfb7d8b1dfdcaa
herits01 <israeletsano@gmail.com>
2022-10-19 22:43:49 -0700

    2-print_alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,9 @@
 void print_alphabet_x10(void)
 {
 		char i, k;
-	for (k = 0; k < 10; k++)
-					{
+		for (k = 0; k < 10; k++)			{
 						for (i = 'a'; i <= 'z'; i++)
-						{
-							_putchar(i);
+						{				_putchar(i);
 									}
 									_putchar('\n');
 											}
