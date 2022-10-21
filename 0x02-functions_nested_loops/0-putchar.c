f6bb60227490de47d948b231125f427cc9e3292b
herits01 <israeletsano@gmail.com>
2022-10-21 15:45:24 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- *  main - data entry point
- *  return: always return 0 (success) 
- **/
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar('_');
-		putchar('p');
-		_putchar('u');
-		putchar('t');
-		_putchar('c');
-		putchar('h');
-		_putchar('a');
-		putchar('r');
-		_putchar('\n');
-return (0);
-				}
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
