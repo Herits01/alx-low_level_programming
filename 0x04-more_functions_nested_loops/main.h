31eaea30b699ca4c6d226f80d9562e8941052ef4
herits01 <israeletsano@gmail.com>
2022-10-25 16:13:31 -0700

    task tests

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-=======
 #include <stdio.h>
 #include "main.c0"
-int _putchar(char c);
+int _putchar( char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -26,4 +12,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
->>>>>>> ba183bc46097bf7eca5284c74431c3c49fc5661c
