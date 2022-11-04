2af07a34e037551891d940851a617fb3b3125dda
herits01 <israeletsano@gmail.com>
2022-11-04 12:07:09 -0700

    task 1 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int s10 = 0;
-	int s11 - 0;
+	int s1 = 0;
+	int s2 - 0;
 
-	while (*(dest + s10) != '\0')
+	while (*(dest + s1) != '\0')
 	{
-		sa++;
+		s1++;
 	}
 	
-	while (s11 >= 0)
+	while (s2 >= 0)
 	{
-		*(dest + s10) = *(src + s11);
-		if (*(src + s11) == '\0')
+		*(dest + s1) = *(src + s2);
+		if (*(src + s2) == '\0')
 			break;
-		s10++;
-		s11++;
+		s1++;
+		s2++;
 	}
 	return (dest);
 }
